670b06eaef4a23ed8ba0e8af02948282f7493040
Loulier Guillaume <guillaume.loulier@hotmail.fr>
2018-02-04 15:24:51 +0100

    fix(Spec): fix on specifications

--- spec/App/FormHandler/RegisterTypeHandlerSpec.php
+++ spec/App/FormHandler/RegisterTypeHandlerSpec.php
@@ -16,9 +16,6 @@ namespace spec\App\FormHandler;
 use PhpSpec\ObjectBehavior;
 use Symfony\Component\Workflow\Registry;
 use Doctrine\ORM\EntityManagerInterface;
-use App\Builder\Interfaces\ImageBuilderInterface;
-use App\Helper\Interfaces\ImageUploaderHelperInterface;
-use App\Helper\Interfaces\ImageRetrieverHelperInterface;
 use App\FormHandler\Interfaces\RegisterTypeHandlerInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
@@ -30,22 +27,16 @@ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 class RegisterTypeHandlerSpec extends ObjectBehavior
 {
     /**
-     * @param \PhpSpec\Wrapper\Collaborator|Registry                      $registry
-     * @param ImageBuilderInterface|\PhpSpec\Wrapper\Collaborator         $imageBuilder
-     * @param EntityManagerInterface|\PhpSpec\Wrapper\Collaborator        $entityManager
-     * @param ImageUploaderHelperInterface|\PhpSpec\Wrapper\Collaborator  $imageUploaderHelper
-     * @param \PhpSpec\Wrapper\Collaborator|UserPasswordEncoderInterface  $userPasswordEncoder
-     * @param ImageRetrieverHelperInterface|\PhpSpec\Wrapper\Collaborator $imageRetrieverHelper
+     * @param \PhpSpec\Wrapper\Collaborator|Registry                     $registry
+     * @param EntityManagerInterface|\PhpSpec\Wrapper\Collaborator       $entityManager
+     * @param \PhpSpec\Wrapper\Collaborator|UserPasswordEncoderInterface $userPasswordEncoder
      */
     public function it_implements(
         Registry $registry,
-        ImageBuilderInterface $imageBuilder,
         EntityManagerInterface $entityManager,
-        ImageUploaderHelperInterface $imageUploaderHelper,
-        UserPasswordEncoderInterface $userPasswordEncoder,
-        ImageRetrieverHelperInterface $imageRetrieverHelper
+        UserPasswordEncoderInterface $userPasswordEncoder
     ) {
-        $this->beConstructedWith($registry, $imageBuilder, $entityManager, $imageUploaderHelper, $userPasswordEncoder, $imageRetrieverHelper);
+        $this->beConstructedWith($registry, $entityManager, $userPasswordEncoder);
         $this->shouldImplement(RegisterTypeHandlerInterface::class);
     }
 }
--- spec/App/Subscriber/Form/ProfileImageSubscriberSpec.php
+++ spec/App/Subscriber/Form/ProfileImageSubscriberSpec.php
@@ -15,10 +15,14 @@ namespace spec\App\Subscriber\Form;
 
 use PhpSpec\ObjectBehavior;
 use Symfony\Component\Form\FormEvent;
+use App\Builder\Interfaces\ImageBuilderInterface;
 use Symfony\Component\Translation\TranslatorInterface;
 use App\Helper\Interfaces\ImageUploaderHelperInterface;
+use App\Helper\Interfaces\ImageRetrieverHelperInterface;
 use App\Subscriber\Interfaces\ProfileImageSubscriberInterface;
+use App\Helper\Interfaces\CloudVision\CloudVisionVoterHelperInterface;
 use App\Helper\Interfaces\CloudVision\CloudVisionAnalyserHelperInterface;
+use App\Helper\Interfaces\CloudVision\CloudVisionDescriberHelperInterface;
 
 /**
  * Class ProfileImageSubscriberSpec.
@@ -28,63 +32,95 @@ use App\Helper\Interfaces\CloudVision\CloudVisionAnalyserHelperInterface;
 class ProfileImageSubscriberSpec extends ObjectBehavior
 {
     /**
-     * @param \PhpSpec\Wrapper\Collaborator|TranslatorInterface                $translator
-     * @param ImageUploaderHelperInterface|\PhpSpec\Wrapper\Collaborator       $imageUploaderHelper
-     * @param CloudVisionAnalyserHelperInterface|\PhpSpec\Wrapper\Collaborator $cloudVisionAnalyserHelper
+     * @param \PhpSpec\Wrapper\Collaborator|TranslatorInterface                 $translator
+     * @param ImageBuilderInterface|\PhpSpec\Wrapper\Collaborator               $imageBuilder
+     * @param CloudVisionVoterHelperInterface|\PhpSpec\Wrapper\Collaborator     $cloudVisionVoterHelper
+     * @param ImageUploaderHelperInterface|\PhpSpec\Wrapper\Collaborator        $imageUploaderHelper
+     * @param CloudVisionAnalyserHelperInterface|\PhpSpec\Wrapper\Collaborator  $cloudVisionAnalyserHelper
+     * @param ImageRetrieverHelperInterface|\PhpSpec\Wrapper\Collaborator       $imageRetrieverHelper
+     * @param CloudVisionDescriberHelperInterface|\PhpSpec\Wrapper\Collaborator $cloudVisionDescriberHelper
      */
     public function it_implements(
         TranslatorInterface $translator,
+        ImageBuilderInterface $imageBuilder,
+        CloudVisionVoterHelperInterface $cloudVisionVoterHelper,
         ImageUploaderHelperInterface $imageUploaderHelper,
-        CloudVisionAnalyserHelperInterface $cloudVisionAnalyserHelper
+        CloudVisionAnalyserHelperInterface $cloudVisionAnalyserHelper,
+        ImageRetrieverHelperInterface $imageRetrieverHelper,
+        CloudVisionDescriberHelperInterface $cloudVisionDescriberHelper
     ) {
-        $this->beConstructedWith($translator, $imageUploaderHelper, $cloudVisionAnalyserHelper);
+        $this->beConstructedWith(
+            $translator,
+            $imageBuilder,
+            $cloudVisionVoterHelper,
+            $imageUploaderHelper,
+            $cloudVisionAnalyserHelper,
+            $imageRetrieverHelper,
+            $cloudVisionDescriberHelper
+        );
         $this->shouldImplement(ProfileImageSubscriberInterface::class);
     }
 
     /**
-     * @param TranslatorInterface                $translator
-     * @param ImageUploaderHelperInterface       $imageUploaderHelper
-     * @param CloudVisionAnalyserHelperInterface $cloudVisionAnalyserHelper
+     * @param TranslatorInterface                 $translator
+     * @param ImageBuilderInterface               $imageBuilder
+     * @param CloudVisionVoterHelperInterface     $cloudVisionVoterHelper
+     * @param ImageUploaderHelperInterface        $imageUploaderHelper
+     * @param CloudVisionAnalyserHelperInterface  $cloudVisionAnalyserHelper
+     * @param ImageRetrieverHelperInterface       $imageRetrieverHelper
+     * @param CloudVisionDescriberHelperInterface $cloudVisionDescriberHelper
      */
     public function should_return_events(
         TranslatorInterface $translator,
+        ImageBuilderInterface $imageBuilder,
+        CloudVisionVoterHelperInterface $cloudVisionVoterHelper,
         ImageUploaderHelperInterface $imageUploaderHelper,
-        CloudVisionAnalyserHelperInterface $cloudVisionAnalyserHelper
+        CloudVisionAnalyserHelperInterface $cloudVisionAnalyserHelper,
+        ImageRetrieverHelperInterface $imageRetrieverHelper,
+        CloudVisionDescriberHelperInterface $cloudVisionDescriberHelper
     ) {
-        $this->beConstructedWith($translator, $imageUploaderHelper, $cloudVisionAnalyserHelper);
+        $this->beConstructedWith(
+            $translator,
+            $imageBuilder,
+            $cloudVisionVoterHelper,
+            $imageUploaderHelper,
+            $cloudVisionAnalyserHelper,
+            $imageRetrieverHelper,
+            $cloudVisionDescriberHelper
+        );
         $this::getSubscribedEvents()->shouldBeArray();
         $this::getSubscribedEvents()->shouldContain('onSubmit');
     }
 
     /**
-     * @param TranslatorInterface                $translator
-     * @param ImageUploaderHelperInterface       $imageUploaderHelper
-     * @param CloudVisionAnalyserHelperInterface $cloudVisionAnalyserHelper
-     * @param FormEvent                          $event
+     * @param TranslatorInterface                 $translator
+     * @param ImageBuilderInterface               $imageBuilder
+     * @param CloudVisionVoterHelperInterface     $cloudVisionVoterHelper
+     * @param ImageUploaderHelperInterface        $imageUploaderHelper
+     * @param CloudVisionAnalyserHelperInterface  $cloudVisionAnalyserHelper
+     * @param ImageRetrieverHelperInterface       $imageRetrieverHelper
+     * @param CloudVisionDescriberHelperInterface $cloudVisionDescriberHelper
+     * @param FormEvent                           $event
      */
     public function should_return_void(
         TranslatorInterface $translator,
+        ImageBuilderInterface $imageBuilder,
+        CloudVisionVoterHelperInterface $cloudVisionVoterHelper,
         ImageUploaderHelperInterface $imageUploaderHelper,
         CloudVisionAnalyserHelperInterface $cloudVisionAnalyserHelper,
+        ImageRetrieverHelperInterface $imageRetrieverHelper,
+        CloudVisionDescriberHelperInterface $cloudVisionDescriberHelper,
         FormEvent $event
     ) {
-        $this->beConstructedWith($translator, $imageUploaderHelper, $cloudVisionAnalyserHelper);
+        $this->beConstructedWith(
+            $translator,
+            $imageBuilder,
+            $cloudVisionVoterHelper,
+            $imageUploaderHelper,
+            $cloudVisionAnalyserHelper,
+            $imageRetrieverHelper,
+            $cloudVisionDescriberHelper
+        );
         $this->onSubmit($event)->shouldReturn(null);
-    }
-
-    /**
-     * @param TranslatorInterface                $translator
-     * @param ImageUploaderHelperInterface       $imageUploaderHelper
-     * @param CloudVisionAnalyserHelperInterface $cloudVisionAnalyserHelper
-     * @param FormEvent                          $event
-     */
-    public function should_analyse_file(
-        TranslatorInterface $translator,
-        ImageUploaderHelperInterface $imageUploaderHelper,
-        CloudVisionAnalyserHelperInterface $cloudVisionAnalyserHelper,
-        FormEvent $event
-    ) {
-        $this->beConstructedWith($translator, $imageUploaderHelper, $cloudVisionAnalyserHelper);
-        $this->uploadAndAnalyseImage($event)->shouldReturn(null);
     }
 }
