a611483d07c8d83e436de4efb348f533f4eb6822
Loulier Guillaume <guillaume.loulier@hotmail.fr>
2018-02-05 16:47:26 +0100

    front(Sass): started on frontend

--- src/FormHandler/Interfaces/RegisterTypeHandlerInterface.php
+++ src/FormHandler/Interfaces/RegisterTypeHandlerInterface.php
@@ -24,8 +24,8 @@ use App\Builder\Interfaces\UserBuilderInterface;
 interface RegisterTypeHandlerInterface
 {
     /**
-     * @param FormInterface        $registerForm the RegisterType form
-     * @param UserBuilderInterface $userBuilder  the User which is managed by the form
+     * @param FormInterface        $registerForm the RegisterType Form
+     * @param UserBuilderInterface $userBuilder  the User which is managed by the Form
      *
      * @return bool if the handling process has succeed
      */
